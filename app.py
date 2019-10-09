e95956904421848f8fefd411325779918c5211b8
eecsg <56319469+eecsg@users.noreply.github.com>
2019-10-09 15:10:18 +0300

    Fix h1 and add billing

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    content = '''<h1>vpnicus</h1>'''
+    content = '''<h1>VPNicus</h1>'''
     return render_template('default.html', content=content)
+
+# This part is optional and can be removed
+@app.route('/buy')
+def billing():
+    return 'Soon!'
 
 @app.route('/verify')
 def verify():
