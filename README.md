b13a8b920f4de5cba6d055c8b07df4b9f85188c0
eecsg <56319469+eecsg@users.noreply.github.com>
2019-10-14 14:53:49 +0300

    Some improvements and README

---
+++ README.md
@@ -0,0 +1,12 @@
+# VPNicus
+Tool that you need for starting VPN business.
+
+## Features
+* User management
+* User authentication
+
+## User authentication
+User authentication is implemented to VPNicus by default, but didn't implemented to OpenVPN.
+
+
+User authentication is important if you have more than one OpenVPN server or/and have multiple users.
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+# This part is optional
 @app.route('/')
 def home():
     content = '''<h1>VPNicus</h1>'''
@@ -9,12 +10,15 @@ def home():
 # This part is optional and can be removed
 @app.route('/buy')
 def billing():
-    return 'Soon!'
+    content = '''Soon!'''
+    return render_template('default.html', content=content)
 
-@app.route('/verify')
+# This part is required for authentication
+@app.route('/verify/<token>')
 def verify():
     return 'Soon!'
 
+# This part is required for managing users.
 @app.route('/admin')
 def admin():
     return 'Soon!'
